ed9b1c617d7ff8049462b095421a119acefb7193
Steve Chalco <stevo@Steves-MacBook-Pro.local>
2018-09-10 21:35:38 -0700

    ch1Header

---
+++ chapter1.h
@@ -0,0 +1,15 @@
+//
+//  chapter1.h
+//  CTCI
+//
+//  Created by Steve Chalco on 9/10/18.
+//  Copyright © 2018 Steve Chalco. All rights reserved.
+//
+#ifndef chapter1_h
+#define chapter1_h
+
+using namespace std;
+bool isUnique(string myString, bool *ASCIIset=NULL, unsigned int n=0);
+
+
+#endif /* chapter1_h */
